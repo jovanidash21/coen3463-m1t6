577c88f44e9091c9b0de52dc1d30f8e755d816f7
Jovani Warguez <jwardash21@gmail.com>
2016-12-17 00:09:39 +0800

    Update Member Page

--- routes/jovaniwarguez.js
+++ routes/jovaniwarguez.js
@@ -4,9 +4,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('jovaniwarguez', {
-        title: 'Jovani Warguez',
+        title: 'Jovani Cadorna Warguez',
         username: 'jovanidash21',
-        profilepic: './img/jovaniwarguez.png'
+        profilepic: './img/jovaniwarguez.png',
+        email: 'jwardash21@gmail.com',
+        mobilenumber: '09272471075',
+        facebook: 'https://facebook.com/jovani.cadornawarguez',
+        twitter: 'http://www.twitter.com/jovanidash21',
+        instagram: 'https://instagram.com/jovanidash21',
+        linkedin: 'https://www.linkedin.com/in/jovani-warguez-827a8a11b',
+        github: 'https://github.com/jovanidash21/',
+        wordpress: 'https://jovaniwarguez.wordpress.com/'
     });
 });
 
--- routes/nerylempenio.js
+++ routes/nerylempenio.js
@@ -4,9 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('nerylempenio', {
-        title: 'Neryl Empenio',
+        title: 'Neryl Anne Barbosa Empenio',
         username: 'nerylempenio',
-        profilepic: './img/nerylempenio.png'
+        email: 'neryempenio@ymail.com',
+        mobilenumber: '09352460370',
+        profilepic: './img/nerylempenio.png',
+        facebook: 'https://facebook.com/profile.php?id=100001183719985',
+        twitter: 'http://www.twitter.com/rrazamanaz',
+        instagram: 'https://instagram.com/rrazamanaz',
+        github: 'https://github.com/nerylempenio/'
     });
 });
 
