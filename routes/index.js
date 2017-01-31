556b2eab0564617ab332360ae5cc09f86f3188c2
jovanidash21 <jwardash21@gmail.com>
2017-01-31 21:03:24 +0800

    Update Slider

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', { 
-  	title: 'JN | Module1',
-  	slideshow1: '/img/slideshow/index/1.png',
-    slideshow2: '/img/slideshow/index/2.png',
-    slideshow3: '/img/slideshow/index/3.png'
+  	title: 'Team JN'
   	});
 });
 
--- routes/jovaniwarguez.js
+++ routes/jovaniwarguez.js
@@ -4,9 +4,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('jovaniwarguez', {
         title: 'Jovani Cadorna Warguez',
-        slideshow1: '/img/slideshow/jovaniwarguez/1.png',
-        slideshow2: '/img/slideshow/jovaniwarguez/2.png',
-        slideshow3: '/img/slideshow/jovaniwarguez/3.png',
+        slideshowLink1: 'https://jovanidash21.github.io/warguez/',
+        slideshowLink2: 'http://www.atlasfertilizer.com/',
+        slideshowLink3: 'http://eyeberrypi.cpecareer.com/',
+        slideshowLink4: 'http://cpecareer.com/',
+        slideshowImage1: '/img/slideshow/jovaniwarguez/1.png',
+        slideshowImage2: '/img/slideshow/jovaniwarguez/2.png',
+        slideshowImage3: '/img/slideshow/jovaniwarguez/3.png',
+        slideshowImage4: '/img/slideshow/jovaniwarguez/4.png',
         username: 'jovanidash21',
         profilepic: './img/jovaniwarguez.png',
         email: 'jwardash21@gmail.com',
