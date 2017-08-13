1c617819dca7f807410fc4c80192e8a2646f2550
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-13 11:05:01 +0800

    Convert Tab Spaces To 2

--- routes/jovaniwarguez.js
+++ routes/jovaniwarguez.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('jovaniwarguez', {
-        title: 'Jovani Cadorna Warguez',
-        slideshowLink1: 'https://jovanidash21.github.io/warguez/',
-        slideshowLink2: 'http://www.atlasfertilizer.com/',
-        slideshowLink3: 'http://eyeberrypi.cpecareer.com/',
-        slideshowLink4: 'http://cpecareer.com/',
-        slideshowImage1: '/img/slideshow/jovaniwarguez/1.png',
-        slideshowImage2: '/img/slideshow/jovaniwarguez/2.png',
-        slideshowImage3: '/img/slideshow/jovaniwarguez/3.png',
-        slideshowImage4: '/img/slideshow/jovaniwarguez/4.png',
-        username: 'jovanidash21',
-        profilepic: './img/jovaniwarguez.png',
-        email: 'jwardash21@gmail.com',
-        mobilenumber: '09272471075',
-        facebook: 'https://facebook.com/jovani.cadornawarguez',
-        twitter: 'http://www.twitter.com/jovanidash21',
-        instagram: 'https://instagram.com/jovanidash21',
-        linkedin: 'https://www.linkedin.com/in/jovani-warguez-827a8a11b',
-        github: 'https://github.com/jovanidash21/',
-        codepen: 'http://codepen.io/jovanidash21/',
-        wordpress: 'https://jovaniwarguez.wordpress.com/'
-    });
+  res.render('jovaniwarguez', {
+    title: 'Jovani Cadorna Warguez',
+    slideshowLink1: 'https://jovanidash21.github.io/warguez/',
+    slideshowLink2: 'http://www.atlasfertilizer.com/',
+    slideshowLink3: 'http://eyeberrypi.cpecareer.com/',
+    slideshowLink4: 'http://cpecareer.com/',
+    slideshowImage1: '/img/slideshow/jovaniwarguez/1.png',
+    slideshowImage2: '/img/slideshow/jovaniwarguez/2.png',
+    slideshowImage3: '/img/slideshow/jovaniwarguez/3.png',
+    slideshowImage4: '/img/slideshow/jovaniwarguez/4.png',
+    username: 'jovanidash21',
+    profilepic: './img/jovaniwarguez.png',
+    email: 'jwardash21@gmail.com',
+    mobilenumber: '09272471075',
+    facebook: 'https://facebook.com/jovani.cadornawarguez',
+    twitter: 'http://www.twitter.com/jovanidash21',
+    instagram: 'https://instagram.com/jovanidash21',
+    linkedin: 'https://www.linkedin.com/in/jovani-warguez-827a8a11b',
+    github: 'https://github.com/jovanidash21/',
+    codepen: 'http://codepen.io/jovanidash21/',
+    wordpress: 'https://jovaniwarguez.wordpress.com/'
+  });
 });
 
 module.exports = router;
--- routes/nerylempenio.js
+++ routes/nerylempenio.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('nerylempenio', {
-        title: 'Neryl Anne Barbosa Empenio',
-        slideshowLink1: 'https://github.com/nerylempenio/',
-        slideshowLink2: 'https://web.facebook.com/coresofficial/',
-        slideshowLink3: 'http://esep.7-eleven.com.ph/moodle/login/index.php',
-        slideshowLink4: 'http://cpecareer.com/',
-        slideshowImage1: '/img/slideshow/nerylempenio/1.png',
-        slideshowImage2: '/img/slideshow/nerylempenio/2.png',
-        slideshowImage3: '/img/slideshow/nerylempenio/3.png',
-        slideshowImage4: '/img/slideshow/nerylempenio/4.png',
-        username: 'nerylempenio',
-        profilepic: './img/nerylempenio.png',
-        email: 'neryempenio@ymail.com',
-        mobilenumber: '09352460370',
-        facebook: 'https://facebook.com/profile.php?id=100001183719985',
-        twitter: 'http://www.twitter.com/rrazamanaz',
-        instagram: 'https://instagram.com/rrazamanaz',
-        github: 'https://github.com/nerylempenio/'
-    });
+  res.render('nerylempenio', {
+    title: 'Neryl Anne Barbosa Empenio',
+    slideshowLink1: 'https://github.com/nerylempenio/',
+    slideshowLink2: 'https://web.facebook.com/coresofficial/',
+    slideshowLink3: 'http://esep.7-eleven.com.ph/moodle/login/index.php',
+    slideshowLink4: 'http://cpecareer.com/',
+    slideshowImage1: '/img/slideshow/nerylempenio/1.png',
+    slideshowImage2: '/img/slideshow/nerylempenio/2.png',
+    slideshowImage3: '/img/slideshow/nerylempenio/3.png',
+    slideshowImage4: '/img/slideshow/nerylempenio/4.png',
+    username: 'nerylempenio',
+    profilepic: './img/nerylempenio.png',
+    email: 'neryempenio@ymail.com',
+    mobilenumber: '09352460370',
+    facebook: 'https://facebook.com/profile.php?id=100001183719985',
+    twitter: 'http://www.twitter.com/rrazamanaz',
+    instagram: 'https://instagram.com/rrazamanaz',
+    github: 'https://github.com/nerylempenio/'
+  });
 });
 
 module.exports = router;
